bb5b86bbaa4e7f008116aba4b29a9ffcd97a263b
Marko asus E403 <mm.kujala@gmail.com>
2019-01-02 22:10:45 +0200

    05-02

--- 05-sorting/02-merge-sort/main.go
+++ 05-sorting/02-merge-sort/main.go
@@ -9,7 +9,7 @@ func main() {
 	l := 0              // left pointer
 	r := len(slice) - 1 // right pointer
 	fmt.Println(slice)
-	fmt.Printf("l: %v, r: %v\n", l, r)
+	// fmt.Printf("l: %v, r: %v\n", l, r)
 	sort(slice, l, r)
 }
 
@@ -17,11 +17,13 @@ func sort(a arr, l, r int) {
 	fmt.Printf("splitting l:%v r:%v \n", l, r)
 	if l < r {
 		m := (l + r) / 2
-		fmt.Printf("m: %v\n", m)
+		fmt.Printf("1) l:%v, m:%v, r:%v\n", l, m, r)
 
-		sort(a, l, m)
-		sort(a, m+1, r)
-
+		sort(a, l, m) // continue to row 23 after m=l
+		fmt.Println("-----sort L done-----")
+		fmt.Printf("2) l:%v, m:%v, r:%v\n", l, m, r)
+		sort(a, m+1, r) // continue to row 24 after m+1=r
+		fmt.Println("-----sort R done-----")
 		merge(a, l, m, r)
 	}
 }
